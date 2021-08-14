d5091a7223503baaec83b7b6b452c5f3c9f4a63c
tmax818 <tylermaxwell661@gmail.com>
2021-08-13 19:34:53 -0700

    update link on index

---
+++ learn/02week/code/cc1.py
@@ -0,0 +1,34 @@
+# Turn line 1 into a comment.
+
+# Turn this line into a comment
+
+# There are two errors in line 3. Here is a hint for one of the errors: When you use pairs of quotes to indicate a string, you can use single or double quotes, as long as you use the same kind for both the start quote and the end quote.
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+
+# Read lines 5 and 6, which demonstrate declaring a variable named a and assigning to it a value with an integer data type, then printing its data type to the screen.
+
+a = 1
+print(type(a))
+
+# On line 8, write code to declare a variable named b and assign to it any value of your choice with a float data type. Then on line 9, write code to print the variable b's data type to the screen.
+
+b = 3.14
+print(type(b))
+
+# On line 11, declare a variable named c and assign to it any value of your choice with a string data type. Then also print its data type to the screen on line 12.
+
+c = "spam"
+print(type(c))
+
+# On line 14, declare a variable named d and assign to it the value of a plus b. On line 15, print the value stored in the variable d to the screen. Note, you should print the actual value of d rather than its data type.
+
+d = a + b
+print(d)
+
+
+# Finally, open the integrated terminal in Visual Studio Code. You can do this either by going to the View menu and selecting Terminal, or using the keyboard shortcut Ctrl/Control + ` <-- this character is a backtick character, not a single quote, and you can find it just below your Esc button at the top left of your keyboard.
+# In the terminal, run your code by entering:
+
+# python cc1.py
